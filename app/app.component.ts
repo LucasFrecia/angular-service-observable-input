499f637cf9bc02df62a954a89dfa4e44b86892e2
LucasFrecia <lucasfrecia@gmail.com>
2019-10-09 20:49:26 -0300

    1 file modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
 import { UserNameService } from './user-name.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnDestroy  {
-  userName = 'Angular 5';
-  _subscription_user_name: any;
 
-  constructor(private userNameService : UserNameService) {
-        this._subscription_user_name = this.userNameService.execChange.subscribe((value) => {
-            this.userName= value;
-        });
+  public userName = 'Angular 8';
+  private _subscription_user_name$: any;
+
+  constructor(private _userNameService : UserNameService) {
+    this._subscription_user_name$ = this._userNameService.execChange.subscribe(
+      value => {
+        this.userName= value;
+      }
+    );
   }
 
   changeUserName(name: string) {
-    this.userNameService.userNameChange(name);
+    this._userNameService.userNameChange(name);
   }
 
   ngOnDestroy() {
-    this._subscription_user_name.unsubscribe();
+    this._subscription_user_name$.unsubscribe();
   }
 }
